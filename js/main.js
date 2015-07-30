91e786cb0e3588c03faadbdb801540f27913797c
Sarah Turek <snturek@gmail.com>
2015-07-30 15:38:27 -0600

    all but division functional

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $("span:not(#cancel, #calc)").on("click",
   function(){
     $('#screen').append(
@@ -9,8 +11,28 @@ $('#cancel').on("click",
     $('#screen').html("");
   });
 
-$('#calc').on("click",
-  function(){
-    $('#screen').html("answer"
-      );
-  });
+$('#calc').on("click", function(){
+  var screenResults = $('#screen').html();
+  var equation = screenResults.split();
+
+  if($("equation:contains()")){
+    var print = equation[0].replace(/\x/g, '*');
+    console.log(print);
+    var answer =  eval(print);
+    $('#screen').html(answer);
+
+  }else if($('equation:contains()')){
+    console.log(equation);
+    var print = equation[0].replace(/[\u00F7]/g, '/');
+    var answer =  eval(print);
+    $('#screen').html(answer);
+
+  }else{
+    var answer =  eval(print);
+    $('#screen').html(answer);
+  }
+});
+
+
+
+});
